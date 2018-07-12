e244b8906386cd64cdba2ad6d45df43f7097b3d2
Luke Wilson <luke.wilson@toptal.com>
2018-07-12 16:57:25 +0300

    Refactor game loop class to extract element-specific logic

--- Ricochet/Ball.cs
+++ Ricochet/Ball.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Ricochet
 {
@@ -7,29 +9,38 @@ namespace Ricochet
     /// </summary>
     public class Ball
     {
-        private const int Gravity = 1;
         private const double BounceRate = 0.95;
         private const double ChangeX = 0.5;
         private const double ChangeY = 2.0;
 
+        private readonly GraphicsDevice _graphicsDevice;
+        private readonly SpriteBatch _spriteBatch;
+
         private readonly int _screenWidth;
         private readonly int _screenHeight;
-        private readonly double _ballRadius;
+        private readonly int _ballRadius;
 
+        private readonly double _gravity;
+
+        private double _x;
+        private double _y;
         private double _changeX;
         private double _changeY;
-
-        public double X { get; private set; }
-        public double Y { get; private set; }
         
-        public Ball(int screenWidth, int screenHeight, double ballRadius)
+        public Ball(
+            GraphicsDevice graphicsDevice, SpriteBatch spriteBatch,
+            int screenWidth, int screenHeight, int ballRadius,
+            double gravity)
         {
+            _graphicsDevice = graphicsDevice;
+            _spriteBatch = spriteBatch;
             _screenWidth = screenWidth;
             _screenHeight = screenHeight;
             _ballRadius = ballRadius;
-            
-            X = _screenWidth / 2.0;
-            Y = _screenWidth / 2.0;
+            _gravity = gravity;
+
+            _x = _screenWidth / 2.0;
+            _y = _screenWidth / 2.0;
         }
 
         public void MoveRight()
@@ -42,38 +53,76 @@ namespace Ricochet
             _changeX -= ChangeX;
         }
 
-        private void ApplyGravity()
-        {
-            if (Math.Abs(_changeY) > 1 || Y < _screenHeight - _ballRadius)
-                _changeY += Gravity;
-        }
-
-        public void MoveBall()
-        {
-            // Move the ball's center
-            X += _changeX;
-            Y += _changeY;
-            
-            // Bounce the ball if needed, and keep in bounds
-            if (Y > _screenHeight - _ballRadius || Y < _ballRadius)
-            {
-                _changeY *= -BounceRate;
-                Y = Math.Max(Math.Min(_screenHeight - _ballRadius, Y), _ballRadius);
-
-            }
-            if (X > _screenWidth - _ballRadius || X < _ballRadius)
-            {
-                _changeX *= -BounceRate;
-                X = Math.Max(Math.Min(_screenWidth - _ballRadius, X), _ballRadius);
-            }
-            
-            // Apply gravity
-            ApplyGravity();
-        }
-
         public void Bounce()
         {
             _changeY -= ChangeY;
+        }
+
+        public void Draw()
+        {
+            UpdateBallPosition();
+
+            var position = new Vector2((float)(_x - _ballRadius), (float)(_y - _ballRadius));
+
+            var diameter = _ballRadius * 2;
+            var colorData = Circle(diameter, Color.Red, Color.Transparent);
+            var texture = new Texture2D(_graphicsDevice, diameter, diameter);
+            texture.SetData(colorData);
+            
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(texture, position, null, Color.White);
+            _spriteBatch.End();
+        }
+
+        private void UpdateBallPosition()
+        {
+            // Move the ball's center
+            _x += _changeX;
+            _y += _changeY;
+            
+            // Bounce the ball if needed, and keep in bounds
+            if (_y > _screenHeight - _ballRadius || _y < _ballRadius)
+            {
+                _changeY *= -BounceRate;
+                _y = Math.Max(Math.Min(_screenHeight - _ballRadius, _y), _ballRadius);
+
+            }
+            if (_x > _screenWidth - _ballRadius || _x < _ballRadius)
+            {
+                _changeX *= -BounceRate;
+                _x = Math.Max(Math.Min(_screenWidth - _ballRadius, _x), _ballRadius);
+            }
+            
+            // Apply gravity
+            if (Math.Abs(_changeY) > 1 || _y < _screenHeight - _ballRadius)
+                _changeY += _gravity;
+        }
+
+        private static Color[] Circle(int diameter, Color fillColour, Color backgroundColour)
+        {
+            var colorData = new Color[diameter * diameter];
+
+            var diam = diameter / 2f;
+            var diamsq = diam * diam;
+
+            for (var x = 0; x < diameter; x++)
+            {
+                for (var y = 0; y < diameter; y++)
+                {
+                    var index = x * diameter + y;
+                    var pos = new Vector2(x - diam, y - diam);
+                    if (pos.LengthSquared() <= diamsq)
+                    {
+                        colorData[index] = fillColour;
+                    }
+                    else
+                    {
+                        colorData[index] = backgroundColour;
+                    }
+                }
+            }
+
+            return colorData;
         }
     }
 }
--- Ricochet/GameLoop.cs
+++ Ricochet/GameLoop.cs
@@ -11,34 +11,33 @@ namespace Ricochet
     {
         private const int ScreenWidth = 700;
         private const int ScreenHeight = 500;
+
+        private const double Gravity = 1;
+
         private const int BallRadius = 25;
-        
-        private GraphicsDeviceManager _graphics;
+
         private SpriteBatch _spriteBatch;
-        private Texture2D _texture;
-        private readonly Ball _ball = new Ball(ScreenWidth, ScreenHeight, BallRadius);
+        private Ball _ball;
 
         public GameLoop()
         {
-            _graphics = new GraphicsDeviceManager(this)
-            {
-                PreferredBackBufferWidth = ScreenWidth,
-                PreferredBackBufferHeight = ScreenHeight
-            };
-            
+            var manager = new GraphicsDeviceManager(this);
+            manager.PreferredBackBufferWidth = ScreenWidth;
+            manager.PreferredBackBufferHeight = ScreenHeight;
+
             Content.RootDirectory = "Content";
         }
 
         protected override void Initialize()
         {
             Window.Title = "Bouncing Balls";
-            _texture = Circle(BallRadius * 2);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _ball = new Ball(GraphicsDevice, _spriteBatch, ScreenWidth, ScreenHeight, BallRadius, Gravity);
         }
 
         protected override void UnloadContent()
@@ -47,24 +46,22 @@ namespace Ricochet
 
         protected override void Update(GameTime gameTime)
         {
-            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (IsKeyDown(Keys.Escape))
             {
                 Exit();
             }
-            else if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            else if (IsKeyDown(Keys.Right))
             {
                 _ball.MoveRight();
             }
-            else if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            else if (IsKeyDown(Keys.Left))
             {
                 _ball.MoveLeft();
             }
-            else if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            else if (IsKeyDown(Keys.Up))
             {
                 _ball.Bounce();
             }
-
-            _ball.MoveBall();
 
             base.Update(gameTime);
         }
@@ -72,41 +69,13 @@ namespace Ricochet
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Black);
-
-            _spriteBatch.Begin();
-            _spriteBatch.Draw(_texture, new Vector2((float)_ball.X - BallRadius, (float)_ball.Y - BallRadius));
-            _spriteBatch.End();
-
+            _ball.Draw();
             base.Draw(gameTime);
         }
-        
-        private Texture2D Circle(int radius)
+
+        private static bool IsKeyDown(Keys key)
         {
-            var texture = new Texture2D(GraphicsDevice, radius, radius);
-            var colorData = new Color[radius*radius];
-
-            var diam = radius / 2f;
-            var diamsq = diam * diam;
-
-            for (var x = 0; x < radius; x++)
-            {
-                for (var y = 0; y < radius; y++)
-                {
-                    var index = x * radius + y;
-                    var pos = new Vector2(x - diam, y - diam);
-                    if (pos.LengthSquared() <= diamsq)
-                    {
-                        colorData[index] = Color.Red;
-                    }
-                    else
-                    {
-                        colorData[index] = Color.Transparent;
-                    }
-                }
-            }
-
-            texture.SetData(colorData);
-            return texture;
+            return Keyboard.GetState().IsKeyDown(key);
         }
     }
 }
