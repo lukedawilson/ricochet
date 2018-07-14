98b48a688533da5e16cd2438274cd1c9a38d21a6
Nathan Varughese <nathan.varughese@gmail.com>
2018-07-14 20:31:33 +0100

    Add images for tiles and an enemy

--- Ricochet/Levels/Level1.cs
+++ Ricochet/Levels/Level1.cs
@@ -8,9 +8,9 @@ namespace Ricochet.Levels
         public Level1(GraphicsDevice graphicsDevice, SpriteBatch spriteBatch)
         {
             var screen1 = new Screen();
-            screen1.AddTile(new Tile(graphicsDevice, spriteBatch, 2, 3, "earth.png"));
-            screen1.AddTile(new Tile(graphicsDevice, spriteBatch, 4, 6, "stars.jpg"));
-            screen1.AddTile(new Tile(graphicsDevice, spriteBatch, 1, 1, "shuttle.png"));
+            screen1.AddTile(new Tile(graphicsDevice, spriteBatch, 2, 3, "bricks1.png"));
+            screen1.AddTile(new Tile(graphicsDevice, spriteBatch, 4, 6, "checker1.png"));
+            screen1.AddTile(new Tile(graphicsDevice, spriteBatch, 1, 1, "enemy_square1.png"));
 
             Screens.Add(screen1);
         }
