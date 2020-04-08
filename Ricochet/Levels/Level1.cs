25c8de31bf18528858a182a0196f2981c88ebd05
Nathan Varughese <nathan.varughese@gmail.com>
2020-04-08 14:05:15 +0100

    level changes

--- Ricochet/Levels/Level1.cs
+++ Ricochet/Levels/Level1.cs
@@ -9,17 +9,177 @@ namespace Ricochet.Levels
         private IDictionary<string, TileFactory> LightBlueMappings => new Dictionary<string, TileFactory>
         {
             { @"CC", new TileFactory<SquareTile>("checker2.png") },
+            { @"/C", new TileFactory<BottomRightTriangleTile>("checker2_BR.png") },
+            { @"C/", new TileFactory<TopLeftTriangleTile>("checker2_TL.png") },
+            { @"\C", new TileFactory<TopRightTriangleTile>("checker2_TR.png") },
+            { @"C\", new TileFactory<BottomLeftTriangleTile>("checker2_BL.png") },
             { @"PP", new TileFactory<SquareTile>("pipes1.png") },
             { @"BB", new TileFactory<SquareTile>("bricks2.png") },
-            { @"/B", new TileFactory<BottomRightTriangleTile>("bricks2_triangle_bottom_right.png") },
-            { @"B/", new TileFactory<TopLeftTriangleTile>("bricks2_triangle_top_left.png") },
-            { @"\B", new TileFactory<TopRightTriangleTile>("bricks2_triangle_top_right.png") },
-            { @"B\", new TileFactory<BottomLeftTriangleTile>("bricks2_triangle_bottom_left.png") },
-            { @"SS", new TileFactory<BottomLeftTriangleTile>("sand_ground1.png") },
+            { @"/B", new TileFactory<BottomRightTriangleTile>("bricks2_BR.png") },
+            { @"B/", new TileFactory<TopLeftTriangleTile>("bricks2_TL.png") },
+            { @"\B", new TileFactory<TopRightTriangleTile>("bricks2_TR.png") },
+            { @"B\", new TileFactory<BottomLeftTriangleTile>("bricks2_BL.png") },
+            { @"SS", new TileFactory<SquareTile>("sand_ground1.png") },
         };
 
         public Level1() : base(Configuration.ScreenWidth, Configuration.ScreenHeight, Configuration.TileDimension)
         {
+            AddScreen(
+                "LLTT",
+                LightBlueMappings,
+                @"CCCCCC    CCCCCCCCCCCCCCCC",
+                @"CCCC      CC            PP",
+                @"CC        CC      CCCCCCCC",
+                @"PP        CC            CC",
+                @"PP        CCCCCCCC      CC",
+                @"CC                      CC",
+                @"CCCC                  CCCC",
+                @"CCCCCC              CCCCCC",
+                @"CCCCCCCCCCCCCCCCCCCCPPPPPP"
+            );
+            AddScreen(
+                "LTT",
+                LightBlueMappings,
+                @"CCCCCC    CCCCCCCCCCCCCCCC",
+                @"CCCC      CC            PP",
+                @"CC        CC      CCCCCCCC",
+                @"PP        CC            CC",
+                @"PP        CCCCCCCC      CC",
+                @"CC                      CC",
+                @"CCCC                  CCCC",
+                @"CCCCCC              CCCCCC",
+                @"CCCCCCCCCCCCCCCCCCCCPPPPPP"
+            );
+            AddScreen(
+                "TT",
+                LightBlueMappings,
+                @"CCCCCC    CCCCCCCCCCCCCCCC",
+                @"CCCC      CC            PP",
+                @"CC        CC      CCCCCCCC",
+                @"PP        CC            CC",
+                @"PP        CCCCCCCC      CC",
+                @"CC                      CC",
+                @"CCCC                  CCCC",
+                @"CCCCCC              CCCCCC",
+                @"CCCCCCCCCCCCCCCCCCCCPPPPPP"
+            );
+            AddScreen(
+                "RTT",
+                LightBlueMappings,
+                @"CCCCCC    CCCCCCCCCCCCCCCC",
+                @"CCCC      CC            PP",
+                @"CC        CC      CCCCCCCC",
+                @"PP        CC            CC",
+                @"PP        CCCCCCCC      CC",
+                @"CC                      CC",
+                @"CCCC                  CCCC",
+                @"CCCCCC              CCCCCC",
+                @"CCCCCCCCCCCCCCCCCCCCPPPPPP"
+            );
+            AddScreen(
+                "RRTT",
+                LightBlueMappings,
+                @"CCCCCC    CCCCCCCCCCCCCCCC",
+                @"CCCC      CC            PP",
+                @"CC        CC      CCCCCCCC",
+                @"PP        CC            CC",
+                @"PP        CCCCCCCC      CC",
+                @"CC                      CC",
+                @"CCCC                  CCCC",
+                @"CCCCCC              CCCCCC",
+                @"CCCCCCCCCCCCCCCCCCCCPPPPPP"
+            );
+            AddScreen(
+                "LLT",
+                LightBlueMappings,
+                @"PP                        ",
+                @"PP                        ",
+                @"PP                        ",
+                @"PP                        ",
+                @"PP                        ",
+                @"PPPPPP                    ",
+                @"PP                        ",
+                @"PP                        ",
+                @"PP                        "
+            );
+            AddScreen(
+                "LT",
+                LightBlueMappings,
+                @"        BBCCCCCCCCCCCCCCCC",
+                @"        BB              CC",
+                @"      /BB/    CC  CCCCCCCC",
+                @"    /BB/      CC        CC",
+                @"    BB    CCCCCCCCCCCCCCCC",
+                @"    BB                  CC",
+                @"    BBBBBB              CC",
+                @"        BB                ",
+                @"        BBBBBBBBBBBBBBBBBB"
+            );
+            AddScreen(
+                "T",
+                LightBlueMappings,
+                @"CC    CCCCCCCC  CCCCCCCCCC",
+                @"CC          CC            ",
+                @"CCCC        CC      CCCCCC",
+                @"CC          CC    CC      ",
+                @"CC          CC  CC        ",
+                @"CCCCCC      CCCCCC      CC",
+                @"CCCCCCCC              CCCC",
+                @"                    CCCCCC",
+                @"CCCCCCCCCCCC    CCCCCCCCCC"
+            );
+            AddScreen(
+                "RT",
+                LightBlueMappings,
+                @"CC  CCC\                CC",
+                @"        \CCCCCCC      /CC/",
+                @"CCCCCC      CCCCCCCC  CC  ",
+                @"      CC    CCCCCCCC  CCCC",
+                @"        CC              CC",
+                @"CCCC          CCCCCCCCCCCC",
+                @"CCCCCC      CCC/          ",
+                @"CCCCCCCC            CCCCCC",
+                @"CCCCCCCCCCCCCCCCCCCCCCCCCC"
+            );
+            AddScreen(
+                "RRT",
+                LightBlueMappings,
+                @"CC                      PP",
+                @"CC                      PP",
+                @"CC          BBBB        PP",
+                @"CC                      PP",
+                @"BB                      PP",
+                @"BB                  BBBBPP",
+                @"                        PP",
+                @"BBB\                    PP",
+                @"BBBBBBBBBBBBBBBBBBBBBBBBBB"
+            );
+            AddScreen(
+                "LL",
+                LightBlueMappings,
+                @"PP                        ",
+                @"PP                        ",
+                @"PP                        ",
+                @"PP                        ",
+                @"PP                        ",
+                @"PP                        ",
+                @"PP                        ",
+                @"PP        SSSS            ",
+                @"PP        SSSSSSSSSSSSSSSS"
+            );
+            AddScreen(
+                "L",
+                LightBlueMappings,
+                @"      /BBBBBBBBBBBBBBBBBBB",
+                @"    /BBBBBBBB/          B/",
+                @"    \BBBBBB/          BB  ",
+                @"                  BBBBB/  ",
+                @"              BBBBBBB/    ",
+                @"                          ",
+                @"        CC                ",
+                @"      CCCCCC        CCCCCC",
+                @"SSSSCCCCCCCCCCCCCCCCCCPPPP"
+            );
             AddScreen(
                 "initial",
                 LightBlueMappings,
@@ -34,20 +194,7 @@ namespace Ricochet.Levels
                 @"PPPPCC    BBBBBBBBBBBBBBBB"
             );
             AddScreen(
-                "initial-left",
-                LightBlueMappings,
-                @"PP      /BBBBBBBBBBBBBBBBB",
-                @"PP    /BBBBBB/          B/",
-                @"PP    \BBBB/          BB  ",
-                @"PP                BBBBB/  ",
-                @"PP            BBBBBBB/    ",
-                @"PP                        ",
-                @"PP      CC                ",
-                @"PP    CCCCCC        CCCCCC",
-                @"SSSSCCCCCCCCCCCCCCCCCCPPPP"
-            );
-            AddScreen(
-                "initial-right",
+                "R",
                 LightBlueMappings,
                 @"CCCCCCCCCCCCCCCCCCCCCCCCCC",
                 @"CC                      PP",
@@ -60,7 +207,7 @@ namespace Ricochet.Levels
                 @"PPPPCC  CCCCCCCCCCCCCCPPPP"
             );
             AddScreen(
-                "initial-top",
+                "RR",
                 LightBlueMappings,
                 @"CCPPCCCCCCCCCC  CCCCCCCCCC",
                 @"CC          CC            ",
@@ -73,7 +220,124 @@ namespace Ricochet.Levels
                 @"CCCCCCCCCCCC    CCCCCCCCCC"
             );
             AddScreen(
-                "initial-bottom",
+                "LLB",
+                LightBlueMappings,
+                @"CCPPCCCCCCCCCC  CCCCCCCCCC",
+                @"CC          CC            ",
+                @"CC          CC      CCCCCC",
+                @"CC          CC    CC    PP",
+                @"CC          CC  CC      PP",
+                @"CCCCCC      CCCCCC      CC",
+                @"CCCCCCCC              CCCC",
+                @"PP                  CCCCCC",
+                @"CCCCCCCCCCCC    CCCCCCCCCC"
+            );
+            AddScreen(
+                "LB",
+                LightBlueMappings,
+                @"CCCCCCCCPPPPPPPPPPPPPPPPPP",
+                @"CCCCPPPPPPPPPPPPPPPPPPPPPP",
+                @"CCCCCCCCCCCCCCCCCCCCCCCCCC",
+                @"CC                        ",
+                @"CC  CCCCCCCCCCCCCC  CCCCCC",
+                @"CC  CC        CCCC      CC",
+                @"CC  CCCC      CC    CCCCCC",
+                @"CC    CCCCC   CC  CCCCCCCC",
+                @"CCCCCCCCCCCC  CC  CCCCCCCC"
+            );
+            AddScreen(
+                "B",
+                LightBlueMappings,
+                @"CCCCCC    CCCCCCCCCCCCCCCC",
+                @"CCCC      CC            PP",
+                @"CC        CC      CCCCCCCC",
+                @"          CC            CC",
+                @"          CCCCCCCC      CC",
+                @"CC                      CC",
+                @"CCCC                  CCCC",
+                @"CCCCCC              CCCCCC",
+                @"CCCCCCCCCCCCCCCCCCCCPPPPPP"
+            );
+            AddScreen(
+                "RB",
+                LightBlueMappings,
+                @"CCCCCC    CCCCCCCCCCCCCCCC",
+                @"          CC            PP",
+                @"CCCCCCCCCCCC      CCCCCCCC",
+                @"PP        CC            CC",
+                @"PP        CCCCCCCC      CC",
+                @"CC                      CC",
+                @"CCCC                  CCCC",
+                @"CCCCCC              CCCCCC",
+                @"CCCCCCCCCCCCCCCCCCCCPPPPPP"
+            );
+            AddScreen(
+                "RRB",
+                LightBlueMappings,
+                @"CCCCCC    CCCCCCCCCCCCCCCC",
+                @"CCCC      CC            PP",
+                @"CC        CC      CCCCCCCC",
+                @"PP        CC            CC",
+                @"PP        CCCCCCCC      CC",
+                @"CC                      CC",
+                @"CCCC                  CCCC",
+                @"CCCCCC              CCCCCC",
+                @"CCCCCCCCCCCCCCCCCCCCPPPPPP"
+            );
+            AddScreen(
+                "LLBB",
+                LightBlueMappings,
+                @"CCCCCC    CCCCCCCCCCCCCCCC",
+                @"CCCC      CC            PP",
+                @"CC        CC      CCCCCCCC",
+                @"PP        CC            CC",
+                @"PP        CCCCCCCC      CC",
+                @"CC                      CC",
+                @"CCCC                  CCCC",
+                @"CCCCCC              CCCCCC",
+                @"CCCCCCCCCCCCCCCCCCCCPPPPPP"
+            );
+            AddScreen(
+                "LBB",
+                LightBlueMappings,
+                @"CCCCCC    CCCCCCCCCCCCCCCC",
+                @"CCCC      CC            PP",
+                @"CC        CC      CCCCCCCC",
+                @"PP        CC            CC",
+                @"PP        CCCCCCCC      CC",
+                @"CC                      CC",
+                @"CCCC                  CCCC",
+                @"CCCCCC              CCCCCC",
+                @"CCCCCCCCCCCCCCCCCCCCPPPPPP"
+            );
+            AddScreen(
+                "BB",
+                LightBlueMappings,
+                @"CCCCCC    CCCCCCCCCCCCCCCC",
+                @"CCCC      CC            PP",
+                @"CC        CC      CCCCCCCC",
+                @"PP        CC            CC",
+                @"PP        CCCCCCCC      CC",
+                @"CC                      CC",
+                @"CCCC                  CCCC",
+                @"CCCCCC              CCCCCC",
+                @"CCCCCCCCCCCCCCCCCCCCPPPPPP"
+            );
+            AddScreen(
+                "RBB",
+                LightBlueMappings,
+                @"CCCCCC    CCCCCCCCCCCCCCCC",
+                @"CCCC      CC            PP",
+                @"CC        CC      CCCCCCCC",
+                @"PP        CC            CC",
+                @"PP        CCCCCCCC      CC",
+                @"CC                      CC",
+                @"CCCC                  CCCC",
+                @"CCCCCC              CCCCCC",
+                @"CCCCCCCCCCCCCCCCCCCCPPPPPP"
+            );
+            AddScreen(
+                "RRBB",
                 LightBlueMappings,
                 @"CCCCCC    CCCCCCCCCCCCCCCC",
                 @"CCCC      CC            PP",
@@ -86,14 +350,17 @@ namespace Ricochet.Levels
                 @"CCCCCCCCCCCCCCCCCCCCPPPPPP"
             );
 
+
             CurrentScreenKey = "initial";
         }
 
         protected override string[,] ScreensLayout => new[,]
         {
-            { null,           "initial-top",    null            },
-            { "initial-left", "initial",        "initial-right" },
-            { null,           "initial-bottom", null            }
+            {   "LLTT",     "LTT",      "TT",       "RTT",      "RRTT" },
+            {   "LLT",      "LT",       "T",        "RT",       "RRT"  },
+            {   "LL",       "L",        "Initial",  "R",        "RR"   },
+            {   "LLB",      "LB",       "B",        "RB",       "RRB"  },
+            {   "LLBB",     "LBB",      "BB",       "RBB",      "RRBB" }
         };
     }
 }
